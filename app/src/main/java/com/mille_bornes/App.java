165fef172ff1e65b2fdee82a1234283582d76271
MarcelD@l@n <marcelxpwong@gmail.com>
2024-09-20 09:28:55 +0200

    modification app.java

--- app/src/main/java/com/mille_bornes/App.java
+++ app/src/main/java/com/mille_bornes/App.java
@@ -3,6 +3,6 @@ package com.mille_bornes;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World   ♕♕!");
     }
 }
